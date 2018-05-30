2190fb116f55c3ee3523a505f0f3704fed14af93
Johanil <32468569+Johanil@users.noreply.github.com>
2018-05-30 04:39:10 +0200

    databas

---
+++ varldstradet.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Värd: 127.0.0.1
+-- Tid vid skapande: 30 maj 2018 kl 04:38
+-- Serverversion: 10.1.32-MariaDB
+-- PHP-version: 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databas: `varldstradet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `comment`
+--
+
+CREATE TABLE `comment` (
+  `comment_id` int(255) NOT NULL,
+  `comment_com` varchar(256) NOT NULL,
+  `user_first` varchar(256) NOT NULL,
+  `user_last` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumpning av Data i tabell `comment`
+--
+
+INSERT INTO `comment` (`comment_id`, `comment_com`, `user_first`, `user_last`) VALUES
+(3, 'sdfsdf', 'Johan', 'Israelsson'),
+(4, 'Hej!', 'Johan', 'Israelsson');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(255) NOT NULL,
+  `user_email` varchar(256) NOT NULL,
+  `user_first` varchar(256) NOT NULL,
+  `user_last` varchar(256) NOT NULL,
+  `user_uname` varchar(256) NOT NULL,
+  `user_pwd` varchar(256) NOT NULL,
+  `user_salt` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumpning av Data i tabell `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_email`, `user_first`, `user_last`, `user_uname`, `user_pwd`, `user_salt`) VALUES
+(1, 'johan4411@gmail.com', 'Johan', 'Israelsson', 'admin', 'b54d1278f6664c86af71a5736d4f04b1949fbcd17b83da9ecb6c3e1a4f8483ee', 'L@2!aZLoe8/U1(!K&…vF');
+
+--
+-- Index för dumpade tabeller
+--
+
+--
+-- Index för tabell `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `user_first` (`user_first`(255),`user_last`(255));
+
+--
+-- Index för tabell `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT för dumpade tabeller
+--
+
+--
+-- AUTO_INCREMENT för tabell `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `comment_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT för tabell `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
